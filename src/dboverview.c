5b8e58fc5368089ef3f99880f37ec6f5b99a6810
lordylordy <lordylordy@3e2ea4e6-2deb-11de-874f-bb9810f3bc79>
2009-07-23 01:45:05 +0000

    check in - sorting works

--- src/dboverview.c
+++ src/dboverview.c
@@ -21,28 +21,53 @@ unsigned int db_overview_hashf(void *rid) {
     return h;
 }
 
-int db_overview_cmp_by_age(DbRowId *rid1,DbRowId *rid2) {
-    return rid1->date - rid2->date;
+int db_overview_cmp_by_age(DbRowId **rid1,DbRowId **rid2) {
+    return (*rid1)->date - (*rid2)->date;
 }
 
-int db_overview_cmp_by_title(DbRowId *rid1,DbRowId *rid2) {
+int db_overview_cmp_by_title(DbRowId **rid1,DbRowId **rid2) {
 
     int c;
     // If titles are different - return comparison
-    if ((c=strcmp(rid1->title,rid2->title)) != 0) {
+    if ((c=strcmp((*rid1)->title,(*rid2)->title)) != 0) {
         return c;
     }
 
-    if (rid1->category == 'T' && rid2->category=='T') {
+    if ((*rid1)->category == 'T' && (*rid2)->category=='T') {
         // Compare by season
-        return (rid1->season - rid2->season);
+        return ((*rid1)->season - (*rid2)->season);
     } else {
         // Same title - arbitrary comparison
-        return rid1 - rid2;
+        return (*rid1) - (*rid2);
     }
 }
 int db_overview_name_eqf(void *rid1,void *rid2) {
-    return db_overview_cmp_by_title(rid1,rid2) == 0;
+    return db_overview_cmp_by_title(&rid1,&rid2) == 0;
+}
+
+void overview_dump(char *label,struct hashtable *overview) {
+    struct hashtable_itr *itr;
+    DbRowId *k;
+
+    if (hashtable_count(overview) ) {
+        for (itr=hashtable_loop_init(overview) ; hashtable_loop_more(itr,&k,NULL) ; ) {
+
+            html_log(1,"%s key=[%c %s s%02d]",label,k->category,k->title,k->season);
+        }
+    } else {
+        html_log(1,"%s EMPTY",label);
+    }
+}
+
+void overview_array_dump(char *label,DbRowId **arr) {
+    if (*arr) {
+        while (*arr) {
+            html_log(1,"%s key=[%c\t%s\ts%02d\t%d]",label,(*arr)->category,(*arr)->title,(*arr)->season,(*arr)->date);
+            arr++;
+        }
+    } else {
+        html_log(1,"%s EMPTY",label);
+    }
 }
 
 
@@ -77,9 +102,11 @@ struct hashtable *db_overview_hash_create(DbRowSet **rowsets) {
         }
     }
     html_log(0,"overview: %d entries created from %d records",hashtable_count(overview),total);
-    hashtable_dump("overview",overview);
+    overview_dump("ovw create:",overview);
     return overview;
 }
+
+
 
 DbRowId **flatten_hash_to_array(struct hashtable *overview) {
     DbRowId **ids = MALLOC((hashtable_count(overview)+1) * sizeof(DbRowId *) );
@@ -100,7 +127,10 @@ DbRowId **sort_overview(struct hashtable *overview, int (*cmp_fn)(const void *,c
 
     DbRowId **ids = flatten_hash_to_array(overview);
 
+    html_log(0,"sorting %d items",hashtable_count(overview));
+    overview_array_dump("ovw flatten",ids);
     qsort(ids,hashtable_count(overview),sizeof(DbRowId *),cmp_fn);
+    overview_array_dump("ovw sorted",ids);
 
     return ids;
 }

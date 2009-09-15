ec01dd5bd7af7aa016a25ae305609bcaad7eb6fe
lordylordy <lordylordy@3e2ea4e6-2deb-11de-874f-bb9810f3bc79>
2009-09-15 22:54:21 +0000

    always display title text

--- trunk/src/display.c
+++ trunk/src/display.c
@@ -1080,9 +1080,9 @@ int unwatched_count(DbRowId *rid) {
 char *mouse_or_focus_event(char *title,char *source,char *on_event,char *off_event) {
     char *result = NULL;
     if (source && *source != '*') {
-        ovs_asprintf(&result," %s=\"show('%s [%s]');\" %s=\"show('');\"",on_event,title,source,off_event);
+        ovs_asprintf(&result," %s=\"show('%s [%s]');\" %s=\"show('_');\"",on_event,title,source,off_event);
     } else {
-        ovs_asprintf(&result," %s=\"show('%s');\" %s=\"show('');\"",on_event,title,off_event);
+        ovs_asprintf(&result," %s=\"show('%s');\" %s=\"show('_');\"",on_event,title,off_event);
     }
     return result;
 }

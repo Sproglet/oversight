7d85d0c814a32e238b6cec047e7be9fa65025805
lordylordy <lordylordy@3e2ea4e6-2deb-11de-874f-bb9810f3bc79>
2009-11-29 13:20:39 +0000

    r571-stable

---
+++ tags/r571-stable/options.sh
@@ -0,0 +1,254 @@
+#!/bin/sh
+VERSION=20090605-1BETA
+
+APPDIR=$( echo $0 | sed -r 's|[^/]+$||' )
+APPDIR=$(cd "${APPDIR:-.}" ; pwd )
+
+#echo "APPDIR=$APPDIR"
+
+#$1 = file
+#$2 = variable
+#$3=value
+OPTION_SET() {
+    vPatt=`echo "$2" | sed -r 's/([][])/\\\\\1/g'` #escape square brackets a[b] to a\[b\]
+    if grep -q "^$vPatt=" "$1" ; then
+        cp "$1" "$1.old"
+
+        sed "s^$vPatt=.*$2="'"'"$3"'"' "$1.old" > "$1"
+    else
+        echo "$2="'"'"$3"'"' >> "$1"
+    fi
+}
+
+# $1=help file format <option name:help text|choice1|..|choicen>
+# $2=cfg file  format <option name=value>
+OPTION_GET() {
+    OPTION_PARSE MODE=GET "$@"
+}
+
+
+# $1=help file format <option name:help text|choice1|..|choicen>
+# $2=cfg file  format <option name=value>
+# $3 optional awk variables eg HIDE_VAR_PREFIX=unpak_ will remove unpak_ prefix from displayed names.
+OPTION_TABLE() {
+    OPTION_PARSE MODE=TABLE "$@"
+}
+OPTION_TABLE2() {
+    OPTION_PARSE MODE=TABLE2 "$@"
+}
+
+OPTION_PARSE() {
+    awk '
+{
+    print "<!-- "FILENAME " : " $0 "-->";
+}
+/^#/ { next }
+
+FILENAME ~ "help$" {
+     addVal(clean($0),":",help,1);
+     next;
+}
+
+FILENAME ~ "(cfg|defaults)$" {
+    addVal(clean($0),"=",val,0);
+    next;
+}
+
+function clean(line,\
+i) {
+    sub(/\r$/,"",line);
+    i=index(line,"#");
+    if ( i > 0 ) { line = substr(line,1,i-1); }
+    return line;
+}
+
+function addVal(line,sep,arr,addToOrder,   i,name,rest) {
+
+    if (MODE ~ "^TABLE") {
+        print "<!-- "FILENAME " : " sep " : " $0 "-->";
+    }
+    sub(/$/,"",line);
+    i = index(line,sep);
+    if (i > 0 ) {
+        name=trim(substr(line,1,i-1));
+        rest=trim(substr(line,i+1));
+        if (addToOrder && !(name in help)) {
+            order = order "|" name ;
+        }
+        arr[name] = rest;
+        print "<!-- "name"="rest" -->";
+    }
+}
+function trim(x) {
+    sub(/^[ "'"'"']+/,"",x);
+    sub(/[ "'"'"']+$/,"",x);
+    return x;
+}
+function html_table(    i,n,current,sel,opts,optCount) {
+    i=1;
+    print "<table class=\"options\" width=\"100%\" >";
+
+    if (MODE=="TABLE") {
+    print "<tr class=\"optionrow\" ><th class=\"optionname\" width=20% >Option</td><th class=\"optionval\" width=40% >Value</td><th class=\"optionhelp\" width=40% >Description</td></tr>";
+    } else {
+        print "<tr class=\"optionrow\" ><th class=\"optionname\" >Option</td><th class=\"optionhelp\" >Description</td></tr>";
+    }
+
+    split(substr(order,2),ord,"|");
+
+    for(i=1 ; ord[i] != "" ; i++) {
+
+        n=ord[i];
+
+        current=(n in val)?val[n]:""
+
+        draw_choice="";
+        if (n in options) {
+
+            split(options[n],opts,"|");
+            optCount=0;
+            for(o in opts) optCount++;
+
+            if (optCount > 0 && !(n in val) ) {
+                #Take default from first option
+                current = val[n]=opts[1];
+            }
+            if (optCount > 1) {
+                draw_choice = sprintf("<select name=option_%s>",n);
+                for(o=1 ; o <= optCount ; o++) {
+                    if (opts[o] == current) {
+                        sel=" selected=\"selected\"";
+                    } else {
+                        sel="";
+                    }
+                    # if the value = "display=>&htmlparam=val" then only display the "display" bit.
+                    display_text = opts[o];
+                    display_and_value=index(opts[o],"=>");
+                    if ( display_and_value ) {
+                        # just use the inital part for the displayed text.
+                        display_text = substr(display_text,1,display_and_value-1);
+                    }
+                    draw_choice = draw_choice sprintf("\t<option value=\"%s\" %s>%s</option>\n",opts[o],sel,display_text);
+                }
+                draw_choice = draw_choice "</select>";
+            }
+
+        }
+        if (draw_choice == "") {
+            draw_choice = sprintf("<input type=text size=20 name=option_%s value=\"%s\" >",n,current);
+        }
+
+        shortName=n;
+        if (HIDE_VAR_PREFIX != "" ) {
+           shortName=substr(n,index(n,"_")+1);
+       }
+        print "<tr class=optionrow"(i%2)"><td class=optionname><b>"shortName"</b>";
+#        if (ENVIRON["REMOTE_ADDR"] != "127.0.0.1") {
+#            printf "(%s)",(n in val)?val[n]:"unset";
+#        }
+        print "<input type=hidden name=orig_option_"n" value=\""val[n]"\" >";
+
+        if (MODE == "TABLE") {
+            print "</td><td class=optionval>";
+        } else {
+            print "<br>";
+        }
+
+        print draw_choice;
+        print "<td class=optionhelp>"help[n]"</td></tr>";
+        q="'"'"'";
+        #print "<td class=optionhelp><a href=\"javascript:alert("q help[n] q");\" >?</a></td></tr>";
+    }
+    print "</table>";
+}
+
+#Return file name with shell meta-chars escaped.
+function quoteFile(f,
+    j,ch) {
+    if (index(f,"\"") == 0) {
+        return "\""f"\"";
+    } else if (index(f,"'"'"'") == 0) {
+        return "'"'"'"f"'"'"'";
+    } else {
+        meta=" !&[]*()\"'"'"'";
+        for(j= 1 ; j <= length(meta) ; j++ ) {
+            ch=substr(meta,j,1);
+            if (index(f,ch)) {
+                #DEBUG("Escaping ["ch"] from "f);
+                gsub("["ch"]","\\"ch,f);
+                #DEBUG("= "f);
+                }
+        }
+        return f;
+    }
+}
+
+function shell_assign() {
+    for (n in help) {
+        if (n in val) {
+            v=val[n];
+        } else {
+            split(options[n],opts,"|");
+            v=opts[1];
+        }
+        printf "option_%s=%s;",n,quoteFile(v);
+    }
+    #Also do additional options in the cfg but not in the help
+    for (n in val) {
+        if(!(n in help)) {
+            printf "option_%s=%s;",n,quoteFile(val[n]);
+        }
+    }
+}
+
+END {
+    for (n in help) {
+        i=index(help[n],"|");
+        if (i > 0 ) {
+            options[n]=substr(help[n],i+1);
+            help[n]=substr(help[n],1,i-1);
+
+        }
+    }
+    if (index(MODE,"TABLE") ) {
+        html_table();
+    } else if (MODE == "GET") {
+        shell_assign();
+    } else {
+        print "ERROR";
+    }
+}
+' "$@"
+}
+
+func=$1
+shift
+case "$func" in
+    TABLE) OPTION_TABLE "$@" ;; ## help config defaults
+    TABLE2) OPTION_TABLE2 "$@" ;;
+    GET) OPTION_GET "$@" ;;
+    SET) OPTION_SET "$@" ;;
+    *) cat <<HERE
+$0 TABLE helpFile configFile
+    Draw html table to set options.
+    help file format <option name:help text|choice1|..|choicen>
+    cfg file  format <option name=value>
+
+$0 TABLE2 helpFile configFile
+   Another table with two columns. name and value in the same html table cell
+
+$0 GET helpFile configFile
+    Disply a line of all settings. This can be passed to eval eg.
+    eval \`$0 GET helpfile configfile\`
+    Note options that are not set in cfg take first value in help as thier default values
+
+$0 SET configFile name value
+HERE
+    ;;
+esac
+
+#OPTION_TABLE test_option.cfg.help test_option.cfg
+#OPTION_SET test_option.cfg free slave2
+#OPTION_SET test_option.cfg slave prisoner
+
+# vi:shiftwidth=4:tabstop=4:expandtab
---
+++ tags/r571-stable/upgrade.sh
@@ -0,0 +1,175 @@
+#!/bin/sh
+# $Id$ 
+#Cant use named pipes due to blocking at script level
+
+#######################################################
+# THIS IS ALL BROKEN For a number of reasons.
+# Main one - hdx has no de-compression tools.
+# no unzip or gunzip - only common tool is unrar
+# but I dont want to create rar files as they 
+# are not free.
+####################################################
+# Now csi uses tar wrapped in zip so we should
+# standardise on zip for simplicity.
+# but it means hdx/egreat users must instally a newer busybox
+# #################################################
+EXE=$0
+while [ -L "$EXE" ] ; do
+    EXE=$( ls -l "$EXE" | sed 's/.*-> //' )
+done
+APPDIR=$( echo $EXE | sed -r 's|[^/]+$||' )
+APPDIR=$(cd "${APPDIR:-.}" ; pwd )
+TVMODE=`cat /tmp/tvmode`
+OWNER=nmt
+GROUP=nmt
+cd "$APPDIR"
+
+PERMS() {
+    chown $OWNER:$GROUP "$1" "$1"/*
+}
+
+HTML() {
+    echo "<p>$@<p>"
+}
+
+
+site="http://prodynamic.co.uk/nmt/"
+site="http://$appnname.googlecode.com/svn/trunk/packages/"
+
+backupdir="$APPDIR.backup_undo"
+
+# $1 = remove version file
+get_new_version() {
+    rm -f $$.v
+    if wget -q -O $$.v "$site/$1" ; then
+        cat $$.v
+    else
+        echo ""
+    fi
+    rm -f $$.v
+}
+
+
+UPGRADE() {
+
+    appname="$1"
+
+    cd "$APPDIR"
+
+    new_version_file=version.dl
+
+    echo "<p>Start $1<p>"
+    case "$2" in 
+        check_stable)
+            v=`get_new_version $1.version`
+            if [ -n "$v" ] ; then
+                echo $v > $new_version_file
+            else
+                echo ERROR > $new_version_file
+            fi
+            chown -R $OWNER:$GROUP $new_version_file
+            ;;
+        check_stable_or_beta)
+            #Check both beta and offical releases.
+            v=`get_new_version $1.version`
+            vb=`get_new_version $1.beta.version`
+
+            #use awk for ordered string compare
+            echo | awk '
+            END {
+  v="'"$v"'";
+  vb="'"$vb"'";
+  if (v > vb ) {
+      print v;
+  } else if (vb != "" ) {
+    print vb;
+  } else {
+    print "ERROR";
+  }
+}' > $new_version_file
+            chown -R $OWNER:$GROUP $new_version_file
+            ;;
+        re-install|install)
+            #This is not a first time install but just to overwrite files with
+            # downloaded ones. see install-cgi for first time install
+            NEWVERSION=`cat $new_version_file`
+            tardir="$APPDIR/versions"
+            newtgzfile="$appname-$NEWVERSION.tgz" 
+            newtarfile="$appname-$NEWVERSION.tar" 
+
+            if [ ! -d "$tardir" ] ; then mkdir p "$tardir" ; fi
+            PERMS .
+
+            #Get new
+            HTML Fetch $site/$appname/$newtgzfile 
+            rm -f -- "$tardir/$newtgzfile"
+            if ! wget -q  -O "$tardir/$newtgzfile" "$site/$appname/$newtgzfile" ; then
+                echo "ERROR getting $site/$appname/$newtgzfile" > $new_version_file;
+                PERMS .
+                exit 1
+            fi
+
+            $APPDIR/bin/gunzip -f -c "$tardir/$newtgzfile" > "$tardir/$newtarfile"
+            rm -f "$tardir/$newtgzfile"
+
+            HTML Backup old files
+            if [ -d "$backupdir" ] ; then
+                if [ -d "$backupdir.2" ] ; then
+                    rm -fr -- "$backupdir.2"
+                fi
+                mv "$backupdir" "$backupdir.2"
+            fi
+            cp -a "$APPDIR" "$backupdir"
+
+            if [ -f post-update.sh ] ; then rm -f ./post-update.sh || true ; fi
+
+            HTML Unpack new files
+            tar xf "$tardir/$newtarfile"
+            chown -R $OWNER:$GROUP .
+
+            HTML Set Permissions
+            PERMS "$tardir"
+
+            HTML Post Update actions
+            if [ -f post-update.sh ] ; then
+                APPDIR="$APPDIR" ./post-update.sh || true
+                rm -f ./post-update.sh || true
+            fi
+            rm -f $new_version_file
+
+            HTML Upgrade Complete
+            ;;
+
+        undo)
+
+            if [ ! -d "$backupdir" ] ; then
+                echo Cant undo : no folder "$backupdir"
+                return 1;
+            fi
+
+            if [ -d "$backupdir.abort" ] ; then
+                rm -fr -- "$backupdir.abort"
+            fi
+
+            mv "$APPDIR" "$backupdir.abort"
+            mv "$backupdir" "$APPDIR"
+            chown -R $OWNER:$GROUP .
+
+            rm -f $new_version_file
+            HTML Undo Complete
+            ;;
+    esac
+
+}
+
+#
+# DISABLED UNTIL DONE PROPERLY
+#
+#logdir="$APPDIR/logs"
+#
+#mkdir -p "$logdir"
+#
+#UPGRADE "$@" > $logdir/upgrade.$$.log 2>&1
+#
+#chown -R $OWNER:$GROUP $logdir
+#

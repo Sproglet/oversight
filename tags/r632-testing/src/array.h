e07073efac871ff041ad5ed612646c30f14025a7
lordylordy <lordylordy@3e2ea4e6-2deb-11de-874f-bb9810f3bc79>
2009-12-11 19:15:41 +0000

    r632-testing

---
+++ tags/r632-testing/src/array.h
@@ -0,0 +1,28 @@
+#ifndef __ARRAY_H_ALORD__
+#define __ARRAY_H_ALORD__
+
+struct array_str {
+    void **array;
+    int size;
+    int mem_size;
+    void (*free_fn)(void *);
+};
+
+typedef struct array_str Array;
+
+void array_print(char *label,Array *a);
+Array *array_new(void(*fr)(void *) );
+void array_add(Array *a,void *ptr);
+void array_set(Array *a,int idx,void *ptr);
+
+#define ARRAY_FREE(a) do { if (a) array_free(a); } while(0)
+void array_free(Array *a );
+
+
+Array *split(char *s,char *pattern,int reg_opts);
+Array *splitc(char *s,char c);
+int array_strcasecmp(const void *a,const void *b);
+void array_sort(Array *a,int (*fn)(const void *,const void *));
+char *join(Array *a,char *sep);
+
+#endif

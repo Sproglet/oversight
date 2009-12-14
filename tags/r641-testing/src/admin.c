f4a310c4d6a67d3432d515419f1018767a094203
lordylordy <lordylordy@3e2ea4e6-2deb-11de-874f-bb9810f3bc79>
2009-12-14 01:02:33 +0000

    r641-testing

---
+++ tags/r641-testing/src/admin.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "admin.h"
+#include "display.h"
+#include "gaya_cgi.h"
+
+
+void display_admin() {
+
+    char *action = query_val("action");
+
+    HTML_LOG(1,"action = %s",action);
+
+    if (!allow_admin()) {
+
+        printf("admin disabled");
+
+    } else if (EMPTY_STR(action) || strcmp(action,"ask")==0 || strcmp(action,"Cancel")==0) {
+
+        display_template("default","admin",0,NULL);
+
+    } else if (util_starts_with(action,"settings")) {
+        
+        display_template("default","settings",0,NULL);
+
+#define CONFIRM_PREFIX "confirm_"
+    } else if (util_starts_with(action,CONFIRM_PREFIX)) {
+        
+        display_template("default",action+strlen(CONFIRM_PREFIX),0,NULL);
+
+#define TEMPLATE_PREFIX "template_"
+    } else if (util_starts_with(action,TEMPLATE_PREFIX)) {
+        
+        display_template("default",action+strlen(TEMPLATE_PREFIX),0,NULL);
+
+    } else {
+
+        display_template("default","completed",0,NULL);
+    }
+
+}

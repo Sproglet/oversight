dc1370f89584a0e59a6a9b4cb70968f1ad2301ef
lordylordy <lordylordy@3e2ea4e6-2deb-11de-874f-bb9810f3bc79>
2010-04-07 04:40:44 +0000

    dbreader code : incomplete

--- src/dbread.c
+++ src/dbread.c
@@ -1,7 +1,11 @@
 #include <string.h>
-#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <assert.h>
 
+#include "dbread.h"
 #include "util.h"
 
 // present a line at a time (like fgets) but avoid inspecting each line.
@@ -13,44 +17,86 @@
 static char *buf = NULL;
 
 // Provide a faster way of reading index.db. Instead of fgets()...
-// the calling function uses dbget(fp,&p).
 // Because fgets is NOT used, each line is NOT terminated by 'nul'
 // but the calling function must look for runs of \n \r
 
 // start = position last looked at by calling function (initially NULL)
 // returns pointer to start of next item in the buffer.
-char *dbget(FILE *fp,char **start,char **data_end)
+
+
+ReadBuf *dbreader_open(char *name)
+{
+    assert(BUF_SIZE > 2 * MAX_LINE);
+
+    ReadBuf *b;
+   
+    b = MALLOC(sizeof(struct ReadBufStruct));
+    b->fd = open(name,O_RDONLY);
+    b->buf = MALLOC(BUF_SIZE);
+    b->buf_size = BUF_SIZE;
+    b->buf_end = b->buf + b->buf_size;
+
+    b->data_start = b->data_end = b->buf;
+
+    b->eof = 0;
+    return b;
+}
+
+void dbreader_close(ReadBuf *b)
+{
+    close(b->fd);
+    FREE(b->buf);
+    FREE(b);
+}
+
+/*
+ * Read more data. 
+ * This should be called when the calling program has processed up to buf->data_end.
+ * After calling buf->data_start,buf->data_end point to new data block.
+ *
+ * An calling example wpound be
+ *
+ * buf = dbreader_open(filename);
+ * p = buf->data_start;
+ *
+ * while(1) {
+ *
+ *   if (p >= buf->data_end) {
+ *     if ((p = dbreader_advance(buf)) == NULL) {
+ *        END OF FILE
+ *        break;
+ *     }
+ *   }
+ *   ...
+ *
+ * }
+ *
+ * dbreader_close(buf);
+ *
+ */
+int dbreader_advance(char *position,ReadBuf *b)
 {
     static char *buf_end;
-    static int eof=0;
-    if (buf == NULL) {
-        // BUF_SIZE must be greater than 2 * MAX_LINE to allow memcpy
-        assert(BUF_SIZE > 2 * MAX_LINE);
-        assert(*start == NULL);
-        buf = MALLOC(BUF_SIZE);
-        buf_end = buf + BUF_SIZE;
-        *start = buf;
-        *data_end = buf;
-    }
+    assert(b == NULL);
 
-    if (*start >= *data_end ) {
-        if (eof) {
-            *data_end = *start = NULL;
+    if (b->data_start >= b->data_end ) {
+        if (b->eof) {
+            b->data_end = b->data_start = NULL;
         } else {
-            if ((buf_end - *data_end ) < MAX_LINE) {
+            if ((buf_end - b->data_end ) < MAX_LINE) {
                 // move current data to start of buffer.
-                memcpy(*start,buf,*data_end-*start);
-                *data_end += ( buf - *start);
-                *start = buf;
+                memcpy(b->data_start,b->buf,b->data_end-b->data_start);
+                b->data_end += ( b->buf - b->data_start);
+                b->data_start = b->buf;
             }
             // read in as much data as possible.
-            int remain = buf_end-*data_end;
-            int bytes = fread(*data_end,1,remain,fp);
+            int remain = b->buf_end - b->data_end;
+            int bytes = read(b->fd,b->data_end,remain);
             if (bytes < remain) {
-                eof = 1;
+                b->eof = 1;
             }
-            *data_end += bytes;
+            b->data_end += bytes;
         }
     }
-    return *start;
+    return b->data_start;
 }
--- src/dbread.h
+++ src/dbread.h
@@ -1,4 +1,19 @@
 #ifndef __OVS_DBREAD_H__
 #define __OVS_DBREAD_H__
-char *dbget(FILE *fp,char **start,char **data_end)
+
+typedef struct ReadBufStruct {
+    char *data_start;
+    char *data_end;
+    int fd;
+    char *buf;
+    char *buf_end;
+    int buf_size;
+    int max_line;
+    int eof;
+} ReadBuf;
+
+
+ReadBuf *dbreader_open(char *name);
+void dbreader_close(ReadBuf *b);
+int dbreader_advance(char *position,ReadBuf *buf);
 #endif

9410daa074d296b9eabbc052070b9aee641d957a
lordylordy <lordylordy@3e2ea4e6-2deb-11de-874f-bb9810f3bc79>
2010-06-03 21:43:04 +0000

    add js to default

---
+++ trunk/templates/default/js/movie.js
@@ -0,0 +1,43 @@
+// $Id:$
+
+// g_idlist and g_title are set by the movie_listing code in oversight
+var g_info='';
+
+function ovs_delist() {   
+	if (confirm("Delist "+g_title+"?")) {
+          action('delist');
+     }
+}
+
+function ovs_delete() {   
+	if (confirm("Are you sure you want to DELETE the FILES for "+g_title+"?")) {
+          action('delete');
+     }
+}
+
+function set_info(info) {
+    g_info=info;
+}
+
+function ovs_info() {
+    alert(g_info);
+}
+
+function ovs_watched() {
+    action('watch');
+}
+
+function ovs_unwatched() {
+    action('unwatch');
+}
+
+function action(a) {
+    var sep;
+    if (window.location.href.indexOf('?') == -1 ) {
+        sep='?';
+    } else {
+        sep = '&';
+    }
+
+    location.replace(window.location.href + sep + "action="+a+"&actionids="+g_idlist);
+}

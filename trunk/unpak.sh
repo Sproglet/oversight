c45b16c0a9ab9a82a8037c470f420c4e20f875a4
lordylordy <lordylordy@3e2ea4e6-2deb-11de-874f-bb9810f3bc79>
2010-09-10 21:51:40 +0000

    get pictures by default

--- trunk/unpak.sh
+++ trunk/unpak.sh
@@ -1963,7 +1963,7 @@ run_catalog() {
     shift
     if [ -f "$script_folder/catalog.sh" ] ; then
         #User has a correct unpak.cfg file.
-        JOBID="$log_name" "$script_folder/catalog.sh" "$folder" GET_POSTERS GET_FANART GET_PORTRAITS "$@" || true
+        JOBID="$log_name" "$script_folder/catalog.sh" "$folder" "$@" || true
         #create_resume_file "$folder/unpak.resume" "$script_folder/catalog.sh" "$folder" "$@"
     else
         INFO "Catalog script not present in $script_folder"

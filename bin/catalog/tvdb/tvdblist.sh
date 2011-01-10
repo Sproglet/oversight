015fb7a03ac0853e4f5aa88c360e62c5a42775aa
lordylordy <lordylordy@3e2ea4e6-2deb-11de-874f-bb9810f3bc79>
2011-01-10 03:36:48 +0000

    change tvdb script

--- bin/catalog/tvdb/tvdblist.sh
+++ bin/catalog/tvdb/tvdblist.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-exit
 for i in A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 0  1 2 3 4 5 6 7 8 9 ; do
     wget -c -O - "http://thetvdb.com/?string=$i&searchseriesid=&tab=listseries&function=Search" |\
     sed -rn "s/.*tab=series.amp;id=([0-9]+).amp.lid=[0-9]+.>($i[^<]+).*/\1:\2/p" |\
     sort -u > tvdb-$i.list
-    sleep 60
+    sleep 10
 done

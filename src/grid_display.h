c89c77621f68efb176d58ddceb37fa898d638fec
lordylordy <lordylordy@3e2ea4e6-2deb-11de-874f-bb9810f3bc79>
2011-06-11 14:37:59 +0000

    move grid display functions to new c module

---
+++ src/grid_display.h
@@ -0,0 +1,6 @@
+#ifndef _OVERSIGHT_GRID_DISPLAY_H_
+#define _OVERSIGHT_GRID_DISPLAY_H_
+#include "types.h"
+#include "grid.h"
+char *get_grid(long page,GridSegment *gs,DbSortedRows *sorted_rows);
+#endif

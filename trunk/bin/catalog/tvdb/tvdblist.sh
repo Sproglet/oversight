ef0d1548a050cd2a72a2ffab3b3941b36789baad
andy@lordy.org.uk <andy@lordy.org.uk@3e2ea4e6-2deb-11de-874f-bb9810f3bc79>
2012-03-18 02:56:53 +0000

    saved

--- trunk/bin/catalog/tvdb/tvdblist.sh
+++ trunk/bin/catalog/tvdb/tvdblist.sh
@@ -5,6 +5,8 @@ EXE=$0
 while [ -h "$EXE" ] ; do EXE="$(readlink "$EXE")"; done
 DIR="$( cd "$( dirname "$EXE" )" && pwd )"
 
+. $DIR/../../ovsenv
+
 refresh_age=30
 
 all_roman="A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 0 1 2 3 4 5 6 7 8 9"
@@ -27,7 +29,7 @@ esac
 
 
 
-items="`echo "$items" | tr a-z A-Z`"
+items="`echo "$items" | $TR a-z A-Z`"
 
 ext=list
 for i in $items ; do

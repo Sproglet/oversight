be0dba086b5911d235efdf98aa1d7b953746d685
andy@lordy.org.uk <andy@lordy.org.uk@3e2ea4e6-2deb-11de-874f-bb9810f3bc79>
2012-04-29 15:48:10 +0000

    fixed utf8 build

--- src/utf8.c
+++ src/utf8.c
@@ -61,7 +61,10 @@ char *utf8norm(char *s,int len)
     } else {
         // Use COMPOSE rather than DECOMPOSE to preserve composed characters
         uint8_t *intptr;
-        int uerr = utf8proc_map((uint8_t *)s,len,&intptr,UTF8PROC_COMPOSE|UTF8PROC_COMPAT|UTF8PROC_STABLE|UTF8PROC_IGNORE|UTF8PROC_STRIPCC);
+
+        //int uerr = utf8proc_map((uint8_t *)s,len,&intptr,UTF8PROC_COMPOSE|UTF8PROC_COMPAT|UTF8PROC_STABLE|UTF8PROC_IGNORE|UTF8PROC_STRIPCC);
+        int uerr = utf8proc_map((uint8_t *)s,len,&intptr,UTF8PROC_COMPOSE|UTF8PROC_STABLE|UTF8PROC_IGNORE|UTF8PROC_STRIPCC);
+TRACE1;
         if (uerr < 0) {
             out = (char *)intptr;
             if (out == NULL) {
